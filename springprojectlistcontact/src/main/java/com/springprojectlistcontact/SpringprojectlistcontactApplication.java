38bd9a2c556d198960d41fdade788213338636af
Elton Almeida <72717766+Elton-mao@users.noreply.github.com>
2024-02-22 20:51:03 -0300

    initial commit

---
+++ springprojectlistcontact/src/main/java/com/springprojectlistcontact/SpringprojectlistcontactApplication.java
@@ -0,0 +1,13 @@
+package com.springprojectlistcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectlistcontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectlistcontactApplication.class, args);
+	}
+
+}
---
+++ springprojectlistcontact/src/test/java/com/springprojectlistcontact/SpringprojectlistcontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springprojectlistcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectlistcontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

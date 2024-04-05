6035ab34e798c50e5abcccf3a0f25c43b313868e
Elton Almeida <72717766+Elton-mao@users.noreply.github.com>
2024-04-04 22:09:41 -0300

    integração com front

--- springprojectlistcontact/src/main/java/com/springprojectlistcontact/controller/ContactController.java
+++ springprojectlistcontact/src/main/java/com/springprojectlistcontact/controller/ContactController.java
@@ -3,28 +3,34 @@ package com.springprojectlistcontact.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springprojectlistcontact.dto.ContactDTO;
 import com.springprojectlistcontact.model.Contact;
 import com.springprojectlistcontact.service.ContactService;
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/contact")
 public class ContactController {
     @Autowired
     ContactService contactService;
 
     @PostMapping
-    public Contact save(@RequestBody Contact contact){
+    public Contact save(@RequestBody ContactDTO data){
+        Contact contact = new Contact(data.name(), data.email());
         return contactService.saveContact(contact);
     }
-    @GetMapping("/all")
+
+    @GetMapping
+    @CrossOrigin
     public List<Contact> contactList() {
-        List<Contact> contacts =contactService.listAll();
+        List<Contact> contacts = contactService.listAll();
         return contacts;
     }
+    
 }
--- springprojectlistcontact/src/main/java/com/springprojectlistcontact/model/Contact.java
+++ springprojectlistcontact/src/main/java/com/springprojectlistcontact/model/Contact.java
@@ -4,14 +4,29 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Entity
 public class Contact {
+   
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String name;
     private String email; 
+    public Contact(String name, String email) {
+     this.name = name;
+     this.email = email;   
+    }
 }
